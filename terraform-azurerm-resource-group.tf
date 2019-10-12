b17a9730f99d8007f26217c49cf5902ccdfaa013
BenCamareno <bencamareno@live.com>
2019-10-12 17:57:31 +1100

    Update terraform-azurerm-resource-group.tf

--- terraform-azurerm-resource-group.tf
+++ terraform-azurerm-resource-group.tf
@@ -13,52 +13,52 @@ provider "azurerm" {
 }
 
 # Create a resource group if it doesn’t exist
-resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "myResourceGroup"
+resource "azurerm_resource_group" "tfGroup1" {
+    name     = "tfResourceGroup1"
     location = "eastus"
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "tf Dev"
     }
 }
 
 # Create virtual network
-resource "azurerm_virtual_network" "myterraformnetwork" {
-    name                = "myVnet"
+resource "azurerm_virtual_network" "tfNetwork1" {
+    name                = "tfVnet"
     address_space       = ["10.0.0.0/16"]
     location            = "eastus"
-    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+    resource_group_name = "${azurerm_resource_group.tfGroup1.name}"
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "tf Dev"
     }
 }
 
 # Create subnet
-resource "azurerm_subnet" "myterraformsubnet" {
-    name                 = "mySubnet"
-    resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
-    virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
+resource "azurerm_subnet" "tfSubnet1" {
+    name                 = "tfSubnet"
+    resource_group_name  = "${azurerm_resource_group.tfGroup1.name}"
+    virtual_network_name = "${azurerm_virtual_network.tfNetwork1.name}"
     address_prefix       = "10.0.1.0/24"
 }
 
 # Create public IPs
-resource "azurerm_public_ip" "myterraformpublicip" {
-    name                         = "myPublicIP"
+resource "azurerm_public_ip" "tfpublicip1" {
+    name                         = "tfPublicIP"
     location                     = "eastus"
-    resource_group_name          = "${azurerm_resource_group.myterraformgroup.name}"
+    resource_group_name          = "${azurerm_resource_group.tfGroup1.name}"
     allocation_method            = "Dynamic"
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "tf Dev"
     }
 }
 
 # Create Network Security Group and rule
-resource "azurerm_network_security_group" "myterraformnsg" {
-    name                = "myNetworkSecurityGroup"
+resource "azurerm_network_security_group" "tfnsg" {
+    name                = "tfNetworkSecurityGroup"
     location            = "eastus"
-    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+    resource_group_name = "${azurerm_resource_group.tfGroup1.name}"
     
     security_rule {
         name                       = "SSH"
@@ -73,26 +73,26 @@ resource "azurerm_network_security_group" "myterraformnsg" {
     }
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "tf Dev"
     }
 }
 
 # Create network interface
-resource "azurerm_network_interface" "myterraformnic" {
-    name                      = "myNIC"
+resource "azurerm_network_interface" "tfnic" {
+    name                      = "tfNIC"
     location                  = "eastus"
-    resource_group_name       = "${azurerm_resource_group.myterraformgroup.name}"
-    network_security_group_id = "${azurerm_network_security_group.myterraformnsg.id}"
+    resource_group_name       = "${azurerm_resource_group.tfGroup1.name}"
+    network_security_group_id = "${azurerm_network_security_group.tfnsg.id}"
 
     ip_configuration {
-        name                          = "myNicConfiguration"
-        subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
+        name                          = "tfNicConfiguration"
+        subnet_id                     = "${azurerm_subnet.tfSubnet1.id}"
         private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
+        public_ip_address_id          = "${azurerm_public_ip.tfpublicip1.id}"
     }
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "tf Dev"
     }
 }
 
@@ -100,35 +100,35 @@ resource "azurerm_network_interface" "myterraformnic" {
 resource "random_id" "randomId" {
     keepers = {
         # Generate a new ID only when a new resource group is defined
-        resource_group = "${azurerm_resource_group.myterraformgroup.name}"
+        resource_group = "${azurerm_resource_group.tfGroup1.name}"
     }
     
     byte_length = 8
 }
 
 # Create storage account for boot diagnostics
-resource "azurerm_storage_account" "mystorageaccount" {
+resource "azurerm_storage_account" "tfstorageaccount" {
     name                        = "diag${random_id.randomId.hex}"
-    resource_group_name         = "${azurerm_resource_group.myterraformgroup.name}"
+    resource_group_name         = "${azurerm_resource_group.tfGroup1.name}"
     location                    = "eastus"
     account_tier                = "Standard"
     account_replication_type    = "LRS"
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "tf Dev"
     }
 }
 
 # Create virtual machine
-resource "azurerm_virtual_machine" "myterraformvm" {
-    name                  = "myVM"
+resource "azurerm_virtual_machine" "tfvm" {
+    name                  = "tfVM"
     location              = "eastus"
-    resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
-    network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
+    resource_group_name   = "${azurerm_resource_group.tfGroup1.name}"
+    network_interface_ids = ["${azurerm_network_interface.tfnic.id}"]
     vm_size               = "Standard_DS1_v2"
 
     storage_os_disk {
-        name              = "myOsDisk"
+        name              = "tfOsDisk"
         caching           = "ReadWrite"
         create_option     = "FromImage"
         managed_disk_type = "Premium_LRS"
@@ -153,10 +153,10 @@ resource "azurerm_virtual_machine" "myterraformvm" {
 
     boot_diagnostics {
         enabled = "true"
-        storage_uri = "${azurerm_storage_account.mystorageaccount.primary_blob_endpoint}"
+        storage_uri = "${azurerm_storage_account.tfstorageaccount.primary_blob_endpoint}"
     }
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "tf Dev"
     }
 }
