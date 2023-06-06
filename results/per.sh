98ddcbac009c5ea7c1a6be4777b404bcba0755ce
Ben <bencamareno@live.com>
2023-06-06 10:47:45 +1000

    Update and rename per.yml to per.sh

---
+++ results/per.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+API_KEY=""
+
+REPORT_ID=""
+OUTPUT_FILE="report.csv"
+
+# Cloud Conformity API endpoint to get the download URL
+API_ENDPOINT="https://ap-southeast-2.cloudconformity.com/v1/reports/${REPORT_ID}/csv"
+
+# Fetch the download URL using curl and pass the API key as a header
+DOWNLOAD_URL=$(curl -H "Authorization: ApiKey ${API_KEY}" "${API_ENDPOINT}" | jq -r '.url')
+
+# Download the CSV file using curl with the obtained download URL
+curl -o "${OUTPUT_FILE}" "${DOWNLOAD_URL}"
+
+# Check if the download was successful
+if [ $? -eq 0 ]; then
+    echo "CSV file downloaded successfully."
+else
+    echo "Failed to download the CSV file."
+fi
