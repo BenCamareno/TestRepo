87bcc18d715995a132a3ab52c39db952ad6eb58d
Ben <bencamareno@live.com>
2023-06-13 13:58:46 +1000

    Update day.py

--- results/day.py
+++ results/day.py
@@ -1,23 +1,9 @@
-import datetime
-
-def is_first_weekday_of_month(weekday):
-    today = datetime.date.today()
-    first_day_of_month = datetime.date(today.year, today.month, 1)
-
-    if first_day_of_month.weekday() == weekday and today.day <= 7:
+def first_week_of_month():
+    # Returns true if the current day is within the first week of the month
+    os.environ['TZ'] = 'Australia/Sydney'
+    time.tzset()
+    date = time.strftime('%d', time.localtime())
+    if int(date) <= 7:
         return True
     else:
         return False
-
-# Iterate over the months of the current year
-for month in range(1, 13):
-    # Create a datetime object representing the first day of the month
-    first_day_of_month = datetime.date(datetime.date.today().year, month, 1)
-    weekday = first_day_of_month.weekday()
-    weekday_name = datetime.date.today().strftime('%A')
-
-    # Check if it is the first occurrence of the weekday in the month
-    if is_first_weekday_of_month(weekday):
-        print(f"The first {weekday_name} of {first_day_of_month.strftime('%B')} is {first_day_of_month}")
-    else:
-        print(f"The first {weekday_name} of {first_day_of_month.strftime('%B')} is not {first_day_of_month}")
