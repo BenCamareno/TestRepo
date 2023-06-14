dd13aef4a8a6153524ae8272006e207f00c69e4b
Ben <bencamareno@live.com>
2023-06-14 16:15:14 +1000

    Update metrics.ps1

--- results/metrics.ps1
+++ results/metrics.ps1
@@ -1,83 +1,18 @@
-$customMetricData = @(
-    @{
-        MetricName = "SourceIP"
-        Value = $source
-        Unit = "Count"
-        Dimensions = @(
-            @{
-                Name = "InstanceID"
-                Value = $instanceId
-            }
-            # @{
-            #     Name = "ASG-Name"
-            #     Value = $asgName
-            # }
-            @{
-                Name = "Hostname"
-                Value = $hostname
-            }
-            @{
-                Name = "Name"
-                Value = "SourceIP"
-            }
-            @{
-                Name = "Status"
-                Value = $status
-            }
-        )
-    },
-    @{
-        MetricName = "ReferenceIP"
-        Value = $referenceId
-        Unit = "Count"
-        Dimensions = @(
-            @{
-                Name = "InstanceID"
-                Value = $instanceId
-            }
-            # @{
-            #     Name = "ASG-Name"
-            #     Value = $asgName
-            # }
-            @{
-                Name = "Hostname"
-                Value = $hostname
-            }
-            @{
-                Name = "Name"
-                Value = "ReferenceIP"
-            }
-            @{
-                Name = "Status"
-                Value = $status
-            }
-        )
-    },
-    @{
-        MetricName = "RootDispersion"
-        Value = $rootDispersion
-        Unit = "Seconds"
-        Dimensions = @(
-            @{
-                Name = "InstanceID"
-                Value = $instanceId
-            }
-            # @{
-            #     Name = "ASG-Name"
-            #     Value = $asgName
-            # }
-            @{
-                Name = "Hostname"
-                Value = $hostname
-            }
-            @{
-                Name = "Name"
-                Value = "RootDispersion"
-            }
-            @{
-                Name = "Status"
-                Value = $status
-            }
-        )
-    }
-)
+$status          = Get-Service w32time -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Status
+$ReferenceIP     = [regex]::Match($ntpSyncStatus.Dimensions.ReferenceId, "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}").Value.Replace('.', '')
+$SourceIP        = $ntpSyncStatus.Dimensions.Source.Split(',')[0].Trim().Replace('.', '')
+$leapIndicator   = [int]$ntpSyncStatus.metrics.LeapIndicator
+$rootDispersion  = [double]::Parse($ntpSyncStatus.metrics.RootDispersion.Trim('s')).ToString("##.###############")
+$rootDelay       = [double]::Parse($ntpSyncStatus.metrics.RootDelay.Trim('s')).ToString("##.###############")
+$precision       = [int]$ntpSyncStatus.metrics.Precision
+$pollInterval    = ([int]$ntpSyncStatus.metrics.PollInterval).ToString("##.###############")
+$stratum         = [int]$ntpSyncStatus.metrics.Stratum
+$LastSyncAgo     = ((New-TimeSpan -Start ($ntpSyncStatus.metrics.LastSyncTime) -End (Get-Date)).TotalSeconds).ToString("##.###############")
+$upstreamTime    = & w32tm /stripchart /computer:169.254.169.123 /samples:1 /dataonly
+$match           = [regex]::Match($upstreamTime, '(\d{2}:\d{2}:\d{2}),\s+([-+]?\d+\.\d+s)')
+$TimeDrift       = ([double]$match.Groups[2].Value.Trim('s')).ToString("##.###############")
+$clockErrorBound = [double]$timeDifference + (0.5 * $rootDelay + $rootDispersion).ToString("##.###############")
+$NumberOfPeers   = (w32tm /query /peers | Select-String -Pattern "Peers" | ForEach-Object { ($_ -split ' ')[1] })
+$instanceId      = Invoke-WebRequest -Uri 169.254.169.254/latest/meta-data/instance-id/ -UseBasicParsing | Select-Object -Expand Content
+$hostname        = hostname
+$accountId       = (Invoke-WebRequest -Uri 169.254.169.254/latest/dynamic/instance-identity/document -UseBasicParsing | ConvertFrom-Json).accountId
